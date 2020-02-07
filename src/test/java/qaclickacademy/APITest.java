f025d2878c2f70fed0f26e2f81025a57ebc28ff7
Prasad Sarode <prasadsarode1@gmail.com>
2020-02-07 21:10:33 +0530

    X commited

--- src/test/java/qaclickacademy/APITest.java
+++ src/test/java/qaclickacademy/APITest.java
@@ -10,6 +10,10 @@ public class APITest {
 		System.out.println("postJIRA");
 		System.out.println("postJIRA2");
 		System.out.println("postJIRA3");
+		System.out.println("postJIRA4");
+		System.out.println("postJIRA5");
+		System.out.println("postJIRA6");
+		
 	}
 	
 	@Test
